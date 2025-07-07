d6c29904ce81d95f820f044a4eadeb1bb4d53030
Prajwal M <122041305+Prajwal-M-Rao@users.noreply.github.com>
2025-07-07 23:35:20 +0530

    Delete Patterns directory

--- Patterns/Inverted_RHS_Triangle.py
+++
@@ -1,8 +0,0 @@
-# ---------------- Pattern 3: Inverted Right-Angled Triangle ----------------
-print("\nPattern 3: Inverted Right-Angled Triangle")
-n=int(input("Enter the value: "))
-for i in range(1, n + 1):
-    for j in range(n, i - 1, -1):
-        print("*", end=" ")
-    print()
-
--- Patterns/Solid_Square.py
+++
@@ -1,9 +0,0 @@
-# ---------------- Pattern 1: Solid Square ----------------
-n=int(input("Enter the value"))
-
-for i in range(n):
-    for j in range(n):
-        print("*", end="  ")
-    print()
-
-
